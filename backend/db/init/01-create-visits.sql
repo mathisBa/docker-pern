94aa316a65124edb9be83690df83d2b8550c0036
Mathis Baveye <mathisbaveye@MacBook-Air-de-Mathis.local>
2025-05-15 15:30:11 +0200

    front only

--- backend/db/init/01-create-visits.sql
+++
@@ -1,7 +0,0 @@
--- backend/db/init/
-CREATE TABLE IF NOT EXISTS visits (
-  id          SERIAL PRIMARY KEY,
-  ip          VARCHAR(45) NOT NULL,
-  browser_id  TEXT        NOT NULL,
-  visited_at  TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
